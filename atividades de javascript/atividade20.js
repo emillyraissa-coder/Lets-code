81604f364a4309ca0e1942636f215f2db544c74e
Emilly Raissa <contato.emillyraissa@gmail.com>
2026-08-05 20:48:05 -0300

    atualização

---
+++ atividades de javascript/atividade20.js
@@ -0,0 +1,18 @@
+//nome do aluno
+const nome ="emilly raissa"
+//notas 
+let nota1 = 7.5
+let nota2 = 8.0
+let nota3 = 6.5
+//calculo da media
+let media = (nota1 + nota2 + nota3) / 3;
+//exibe no console a media do aluno
+console.log(`A aluna ${nome} obteve a media ${media.toFixed(2)}`);
+
+//peso e altura
+let peso = 45;
+let altura = 1.52;
+//cálculo do IMC
+let imc = peso / (altura * altura);
+//exibe no console
+console.log(`O IMC é: ${imc.toFixed(2)}`);
